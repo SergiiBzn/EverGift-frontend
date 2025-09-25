d15ccb93a1ffd70e53a214facf48a2ae9ce15a0f
Yan Yang <y.y.erica@hotmail.com>
2025-09-25 10:33:20 +0200

    add env

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,13 @@
 /** @format */
 
-import React, { useState } from "react";
+import { useState } from "react";
 import ReceivedGiftModal from "../components/ReceivedGiftModal.jsx";
+import useAuth from "../hooks/useAuth.jsx";
 
 export default function Profile() {
   const [open, setOpen] = useState(false);
-
+  const { user } = useAuth();
+  console.log(user);
   return (
     <div className="p-6">
       <h1 className="mb-4 text-2xl font-semibold">Profile</h1>

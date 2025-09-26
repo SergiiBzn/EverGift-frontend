6ad78159cdbc226bc7e35ab7a5ee8ba20f8f49cb
Yan Yang <y.y.erica@hotmail.com>
2025-09-26 12:10:59 +0200

    add validate display

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -163,7 +163,7 @@ export default function Profile() {
 
               <button
                 onClick={() => setIsOpenEditProfile(true)}
-                className="w-full rounded bg-primary px-4 py-2 text-sm font-bold text-white md:w-auto"
+                className="btn btn-sm btn-primary"
               >
                 Edit Profile
               </button>

2664dec1e0ba43973feed28a3cb7adcd1acaa956
Yan Yang <y.y.erica@hotmail.com>
2025-09-29 16:15:50 +0200

    modified contact profile avatar upload

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,20 +1,17 @@
 /** @format */
-import useAuth from '../hooks/useAuth.jsx';
+import useAuth from "../hooks/useAuth.jsx";
 
-import ProfileSection from '../components/profile-page/ProfileSection.jsx';
-import ContactSection from '../components/profile-page/ContactSection.jsx';
-import WishlistSection from '../components/profile-page/WishlistSection.jsx';
-import ReceivedGiftSection from '../components/profile-page/ReceivedGiftSection.jsx';
+import ProfileSection from "../components/profile-page/ProfileSection.jsx";
+import ContactSection from "../components/profile-page/ContactSection.jsx";
+import WishlistSection from "../components/profile-page/WishlistSection.jsx";
+import ReceivedGiftSection from "../components/profile-page/ReceivedGiftSection.jsx";
 
 export default function Profile() {
   const { user, allUsers } = useAuth();
 
-  console.log('all not custom users', allUsers);
-  console.log('all users contacts', user.contacts);
-
   return (
-    <div className='mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8'>
-      <div className='flex flex-col gap-12'>
+    <div className="mx-auto max-w-5xl px-4 py-8 sm:px-6 lg:px-8">
+      <div className="flex flex-col gap-12">
         {/* profile */}
         <ProfileSection profile={user.profile} />
 

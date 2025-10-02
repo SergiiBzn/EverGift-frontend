8787d6a4f5749b4b54db79aa8a114b71db4f775a
arnaudbrice <mephisteur@gmail.com>
2025-10-02 09:43:27 +0200

    add reminder ui and logic

--- src/components/dashboard-page/Reminder.jsx
+++ src/components/dashboard-page/Reminder.jsx
@@ -23,9 +23,9 @@ const ReminderComponent = () => {
   return (
     <div className="bg-base-200 p-4 rounded-xl flex flex-col gap-6 shadow-md min-w-[300px]">
       <div>
-        <div className="divider divider-primary "></div>
+        <div className="divider divider-primary " />
         <h3 className="text-lg font-bold mb-4 text-center">Pinned</h3>
-        <div className="divider divider-primary "></div>
+        <div className="divider divider-primary " />
         <div className="space-y-4">
           <div className="flex items-center gap-4 p-4 rounded-lg bg-background-light dark:bg-background-dark shadow-sm">
             <div className="flex-1">
@@ -34,18 +34,18 @@ const ReminderComponent = () => {
                 May 12
               </p>
             </div>
-            <div className="w-12 h-12 bg-center bg-no-repeat bg-cover rounded-full bg-[url('https://lh3.googleusercontent.com/aida-public/AB6AXuCvPSQKPGXCnWbLY35gfN4kooPIpbnyxt-oaqWBmhjmLdxdvXhGqJbKlB8PxPcnbcNWEUdFBf8y8n5u2kWlMRgO56AOe95_eJ2AFig4rG2TmLZDdZtEJzL8Py9h1fu9HA0SP7SJLL7gX5v-7rsyCqzyE6ocNnQBFNXrqcR0TDTnZoKeFi9wXy7N27TJ29XpdTjB6YFnulgCba2kXXU2CejP7fD-5o_7p8exurdt8obuep_pKXhoDca0Etzb52CaMlFoqCqO4pmTTYPh')]"></div>
+            <div className="w-12 h-12 bg-center bg-no-repeat bg-cover rounded-full bg-[url('https://lh3.googleusercontent.com/aida-public/AB6AXuCvPSQKPGXCnWbLY35gfN4kooPIpbnyxt-oaqWBmhjmLdxdvXhGqJbKlB8PxPcnbcNWEUdFBf8y8n5u2kWlMRgO56AOe95_eJ2AFig4rG2TmLZDdZtEJzL8Py9h1fu9HA0SP7SJLL7gX5v-7rsyCqzyE6ocNnQBFNXrqcR0TDTnZoKeFi9wXy7N27TJ29XpdTjB6YFnulgCba2kXXU2CejP7fD-5o_7p8exurdt8obuep_pKXhoDca0Etzb52CaMlFoqCqO4pmTTYPh')]" />
           </div>
         </div>
       </div>
 
       <div className="space-y-4">
-        <div className="divider divider-primary "></div>
+        <div className="divider divider-primary " />
         <h3 className="text-lg font-bold mb-4 text-center">
           Events in the next 2 months
         </h3>
 
-        <div className="divider divider-primary "></div>
+        <div className="divider divider-primary " />
         <div
           className={`space-y-4 overflow-y-auto pr-1`}
           style={{ maxHeight: "450px" }}
